04160a01e862cbbb020728f2dd70e0d4e86e2a00
Kyle Han <kyle@sentbe.com>
2021-08-15 20:48:31 +0900

    feat(parseDecimalPlace): Make parseDecimalPlace in PriceListPresentation

--- get-coin-list-project/src/presentations/PriceListPresentation.ts
+++ get-coin-list-project/src/presentations/PriceListPresentation.ts
@@ -5,6 +5,7 @@ import { IPriceListUseCase, PriceListUseCase } from '@/useCases/PriceListUseCase
 
 export interface IPriceListPresentation {
   getCoinMarketsTable (param: GetCoinMarketsParams): Promise<Array<CoinInfoInTable>>
+  parseDecimalPlace (num: number, place: number): string
   convertResponseToInfoTable (response: Array<GetCoinMarketsResponse>): Array<CoinInfoInTable>
 }
 
@@ -18,16 +19,19 @@ export class PriceListPresentation implements IPriceListPresentation {
     return this.convertResponseToInfoTable(response);
   }
 
+  parseDecimalPlace
+    = (num: number, place: number): string => parseFloat(num.toFixed(place)).toLocaleString()
+
   convertResponseToInfoTable
     = (response: Array<GetCoinMarketsResponse>)
     : Array<CoinInfoInTable> => response.map((info: GetCoinMarketsResponse): CoinInfoInTable => ({
       isBookmarked: false,
       name: info.name,
       symbol: info.symbol,
-      price: `${info.current_price}`,
-      oneHourPercent: `${info.price_change_percentage_1h_in_currency}`,
-      oneDayPercent: `${info.price_change_percentage_24h}`,
+      price: this.parseDecimalPlace(info.current_price, 2),
+      oneHourPercent: this.parseDecimalPlace(info.price_change_percentage_1h_in_currency, 2),
+      oneDayPercent: this.parseDecimalPlace(info.price_change_percentage_24h, 2),
       oneWeekPercent: '100',
-      totalVolume: `${info.total_volume}`,
+      totalVolume: `${info.total_volume.toLocaleString()}`,
     }))
 }
--- get-coin-list-project/src/views/PriceList.vue
+++ get-coin-list-project/src/views/PriceList.vue
@@ -19,24 +19,10 @@ import { IPriceListPresentation, PriceListPresentation } from '@/presentations/P
 export default class PriceList extends Vue {
   priceListPresentation: IPriceListPresentation = new PriceListPresentation()
 
-  // mockTableData: Array<CoinInfoInTable> = [
-  //   {
-  //     isBookmarked: true,
-  //     name: 'Bitcoin',
-  //     symbol: 'btc',
-  //     price: '₩ 46,298,075',
-  //     oneHourPercent: '0.75%',
-  //     oneDayPercent: '0.12%',
-  //     oneWeekPercent: '24.36%',
-  //     oneDayVolume: '₩ 41,496,323,888,705',
-  //   },
-  // ]
-
   tableData: Array<CoinInfoInTable> = []
 
   async created(): Promise<void> {
     this.tableData = await this.priceListPresentation.getCoinMarketsTable({ vs_currency: 'USD', page: 1 });
-    console.log(this.tableData);
   }
 }
 </script>
