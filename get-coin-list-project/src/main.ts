b87cf72dd794a3455d929ee84aa032a4ff1d1e07
Kyle Han <kyle@sentbe.com>
2021-08-16 20:04:12 +0900

    Revert "Revert "docs(font-awesome): Install font-awesome""
    This reverts commit 669f79bcdf26725f17a052416c7bcc55adb7b1b4.

--- get-coin-list-project/src/main.ts
+++ get-coin-list-project/src/main.ts
@@ -1,8 +1,15 @@
 import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import store from './store/index';
 import router from './router';
 import './assets/styles/index.scss';
+
+library.add(faUserSecret);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
